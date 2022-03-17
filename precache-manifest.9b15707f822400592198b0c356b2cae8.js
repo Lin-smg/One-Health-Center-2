274118f92092aa16fccf500865f22cf5849e58b8
Lin-smg <yelinphyocu@gmail.com>
2022-03-17 11:47:36 +0630

    third commit

---
+++ precache-manifest.9b15707f822400592198b0c356b2cae8.js
@@ -0,0 +1,350 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ab44ccdfd2a9c006f17",
+    "url": "/pvue-light-bootstrap-dashboard/css/app.7c4d4a4e.css"
+  },
+  {
+    "revision": "6ab44ccdfd2a9c006f17",
+    "url": "/pvue-light-bootstrap-dashboard/js/app.9085182a.js"
+  },
+  {
+    "revision": "ed64f89b9ff121dbb828",
+    "url": "/pvue-light-bootstrap-dashboard/js/chunk-116a9f48.aa4ec081.js"
+  },
+  {
+    "revision": "15b1e5b5234ea8c963dc",
+    "url": "/pvue-light-bootstrap-dashboard/js/chunk-2d0cfad1.843489d6.js"
+  },
+  {
+    "revision": "e2d928018cb3ba2a805c",
+    "url": "/pvue-light-bootstrap-dashboard/js/chunk-2d219ff9.0aab29fd.js"
+  },
+  {
+    "revision": "ea6efb8be329004be781",
+    "url": "/pvue-light-bootstrap-dashboard/js/chunk-f1fffad8.4eb2e9f7.js"
+  },
+  {
+    "revision": "236437456d5329fcae8e",
+    "url": "/pvue-light-bootstrap-dashboard/css/chunk-vendors.2cb53547.css"
+  },
+  {
+    "revision": "236437456d5329fcae8e",
+    "url": "/pvue-light-bootstrap-dashboard/js/chunk-vendors.a4b8a7fd.js"
+  },
+  {
+    "revision": "f7af8d137f82a8562c8e84712481b48f",
+    "url": "/pvue-light-bootstrap-dashboard/img/blog_4.f7af8d13.jpg"
+  },
+  {
+    "revision": "4786fc944bc10bf02afc18c10968ab9b",
+    "url": "/pvue-light-bootstrap-dashboard/img/doctor_1.4786fc94.jpg"
+  },
+  {
+    "revision": "3bb561d65263974cfa32e8fc2ead3cbf",
+    "url": "/pvue-light-bootstrap-dashboard/img/blog_2.3bb561d6.jpg"
+  },
+  {
+    "revision": "9493aceccfde53fcd42917c005214fbe",
+    "url": "/pvue-light-bootstrap-dashboard/img/bg-doctor.9493acec.png"
+  },
+  {
+    "revision": "94449552d6ebc1342d43de0bf329bcb1",
+    "url": "/pvue-light-bootstrap-dashboard/img/doctor_2.94449552.jpg"
+  },
+  {
+    "revision": "71393cb729c2f3ec4330d0f808b2881d",
+    "url": "/pvue-light-bootstrap-dashboard/img/doctor_3.71393cb7.jpg"
+  },
+  {
+    "revision": "efb20878459884ee1ca83675f4c8a5c4",
+    "url": "/pvue-light-bootstrap-dashboard/img/blog_5.efb20878.jpg"
+  },
+  {
+    "revision": "be48b91c111829c66d92020f344a37f2",
+    "url": "/pvue-light-bootstrap-dashboard/img/google_play.be48b91c.svg"
+  },
+  {
+    "revision": "377f1f3beefd3420f896be73cf75ca6e",
+    "url": "/pvue-light-bootstrap-dashboard/img/blog_1.377f1f3b.jpg"
+  },
+  {
+    "revision": "9368569ac8d6094e2714c9bda8dbc62f",
+    "url": "/pvue-light-bootstrap-dashboard/img/app_store.9368569a.svg"
+  },
+  {
+    "revision": "f6320e717c40f2aa46c846e319f37b3c",
+    "url": "/pvue-light-bootstrap-dashboard/img/mobile_app.f6320e71.png"
+  },
+  {
+    "revision": "8224e0160e362e117cbe00495919e2af",
+    "url": "/pvue-light-bootstrap-dashboard/fonts/nucleo-icons.8224e016.eot"
+  },
+  {
+    "revision": "ff208b8d733e61a630992ee2b72c9c1c",
+    "url": "/pvue-light-bootstrap-dashboard/fonts/nucleo-icons.ff208b8d.woff2"
+  },
+  {
+    "revision": "b0dc05d015e91e7d28d79cd0056fe555",
+    "url": "/pvue-light-bootstrap-dashboard/fonts/nucleo-icons.b0dc05d0.ttf"
+  },
+  {
+    "revision": "ae42fa524981a56f3f289447cff36eb1",
+    "url": "/pvue-light-bootstrap-dashboard/img/nucleo-icons.ae42fa52.svg"
+  },
+  {
+    "revision": "f1de43f318488325655666e655aa16a6",
+    "url": "/pvue-light-bootstrap-dashboard/img/blog_3.f1de43f3.jpg"
+  },
+  {
+    "revision": "d6f3192ff0be318fc3f0e93de895dc4d",
+    "url": "/pvue-light-bootstrap-dashboard/index.html"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/pvue-light-bootstrap-dashboard/.gitkeep"
+  },
+  {
+    "revision": "ec2bc9732b1c9e4491b1d98e7fdb258c",
+    "url": "/pvue-light-bootstrap-dashboard/_redirects"
+  },
+  {
+    "revision": "b02604058889da17a92ea9d65cce6373",
+    "url": "/pvue-light-bootstrap-dashboard/assets/css/bootstrap.css"
+  },
+  {
+    "revision": "034df21ec314ecd977b5b9991e82d630",
+    "url": "/pvue-light-bootstrap-dashboard/assets/css/maicons.css"
+  },
+  {
+    "revision": "58c635f5574bae2ad96a774b168b9a0d",
+    "url": "/pvue-light-bootstrap-dashboard/assets/css/theme.css"
+  },
+  {
+    "revision": "01000918725acebd286de3787fca4ee0",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/ajax-loader.gif"
+  },
+  {
+    "revision": "9368569ac8d6094e2714c9bda8dbc62f",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/app_store.svg"
+  },
+  {
+    "revision": "9ff11aa2830ddf0895cf909f89e094b7",
+    "url": "/pvue-light-bootstrap-dashboard/assets/fonts/maicons.eot"
+  },
+  {
+    "revision": "e4e4a6a45276fa27168c9b4d40e2582b",
+    "url": "/pvue-light-bootstrap-dashboard/assets/fonts/maicons.ttf"
+  },
+  {
+    "revision": "f55664336847b2aad2dfb1f82eeaf7f4",
+    "url": "/pvue-light-bootstrap-dashboard/assets/fonts/maicons.woff"
+  },
+  {
+    "revision": "af2acb0862ef49a0578f498e99645d5e",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/banner-pattern.svg"
+  },
+  {
+    "revision": "3bb561d65263974cfa32e8fc2ead3cbf",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/blog/blog_2.jpg"
+  },
+  {
+    "revision": "f1de43f318488325655666e655aa16a6",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/blog/blog_3.jpg"
+  },
+  {
+    "revision": "377f1f3beefd3420f896be73cf75ca6e",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/blog/blog_1.jpg"
+  },
+  {
+    "revision": "efb20878459884ee1ca83675f4c8a5c4",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/blog/blog_5.jpg"
+  },
+  {
+    "revision": "4786fc944bc10bf02afc18c10968ab9b",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/doctors/doctor_1.jpg"
+  },
+  {
+    "revision": "f7af8d137f82a8562c8e84712481b48f",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/blog/blog_4.jpg"
+  },
+  {
+    "revision": "28a23f21cb80b91ee3177ca4cd228183",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/bg_image_1.jpg"
+  },
+  {
+    "revision": "71393cb729c2f3ec4330d0f808b2881d",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/doctors/doctor_3.jpg"
+  },
+  {
+    "revision": "f6320e717c40f2aa46c846e319f37b3c",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/mobile_app.png"
+  },
+  {
+    "revision": "94449552d6ebc1342d43de0bf329bcb1",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/doctors/doctor_2.jpg"
+  },
+  {
+    "revision": "9493aceccfde53fcd42917c005214fbe",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/bg-doctor.png"
+  },
+  {
+    "revision": "044db8b46e7836de075c141fbf381d09",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/person/person_1.jpg"
+  },
+  {
+    "revision": "5e76041c540f7adaa6277d38370f91ce",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/person/person_2.jpg"
+  },
+  {
+    "revision": "b675acfe1b17d9469b6a42b85ba5302d",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/person/person_3.jpg"
+  },
+  {
+    "revision": "438821386cadbe054efc007b9d99250e",
+    "url": "/pvue-light-bootstrap-dashboard/assets/js/google-maps.js"
+  },
+  {
+    "revision": "3648fa980c6623edee76d548305e5f8a",
+    "url": "/pvue-light-bootstrap-dashboard/assets/js/bootstrap.bundle.min.js"
+  },
+  {
+    "revision": "9ac39dc31635a363e377eda0f6fbe03f",
+    "url": "/pvue-light-bootstrap-dashboard/assets/js/jquery-3.5.1.min.js"
+  },
+  {
+    "revision": "1eb22f86c6ff5acf2eb6c4eca77bda63",
+    "url": "/pvue-light-bootstrap-dashboard/assets/js/theme.js"
+  },
+  {
+    "revision": "be48b91c111829c66d92020f344a37f2",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/google_play.svg"
+  },
+  {
+    "revision": "c9aeb6363c02cf5257f1d8f850ef22f4",
+    "url": "/pvue-light-bootstrap-dashboard/assets/vendor/animate/animate.css"
+  },
+  {
+    "revision": "01000918725acebd286de3787fca4ee0",
+    "url": "/pvue-light-bootstrap-dashboard/assets/vendor/owl-carousel/ajax-loader.gif"
+  },
+  {
+    "revision": "1f05e0c6be3220ebd438f66a8747a57c",
+    "url": "/pvue-light-bootstrap-dashboard/assets/vendor/owl-carousel/css/owl.carousel.css"
+  },
+  {
+    "revision": "47c357c05cb99cedbac2874840319818",
+    "url": "/pvue-light-bootstrap-dashboard/assets/vendor/owl-carousel/js/owl.carousel.min.js"
+  },
+  {
+    "revision": "164b265e6089f412b7927848018ae6a1",
+    "url": "/pvue-light-bootstrap-dashboard/assets/vendor/wow/wow.min.js"
+  },
+  {
+    "revision": "fb99e4afa1bdf7ff54ad733f3869f3e5",
+    "url": "/pvue-light-bootstrap-dashboard/assets/img/person/person_4.jpg"
+  },
+  {
+    "revision": "c1a79ea184d48eac08037a68032ee093",
+    "url": "/pvue-light-bootstrap-dashboard/assets/vendor/owl-carousel/js/owl.carousel.js"
+  },
+  {
+    "revision": "be74132f42ae1d3502f9a9b9fff68ac1",
+    "url": "/pvue-light-bootstrap-dashboard/Dashboard.PNG"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/pvue-light-bootstrap-dashboard/favicon.png"
+  },
+  {
+    "revision": "a9615bac158705203261e8348f574cc8",
+    "url": "/pvue-light-bootstrap-dashboard/img/default-avatar.png"
+  },
+  {
+    "revision": "a9615bac158705203261e8348f574cc8",
+    "url": "/pvue-light-bootstrap-dashboard/img/faces/face-0.jpg"
+  },
+  {
+    "revision": "99e58416b89637502b40ac8350eed85a",
+    "url": "/pvue-light-bootstrap-dashboard/img/faces/face-1.jpg"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/pvue-light-bootstrap-dashboard/img/faces/face-3.jpg"
+  },
+  {
+    "revision": "4b87e628f4ef9988718860890b2a682f",
+    "url": "/pvue-light-bootstrap-dashboard/img/faces/face-6.jpg"
+  },
+  {
+    "revision": "bf1684a30a86ba1b222aab3acff16356",
+    "url": "/pvue-light-bootstrap-dashboard/img/faces/face-7.jpg"
+  },
+  {
+    "revision": "4ab22eca4053c14a34e4bdb6390deae9",
+    "url": "/pvue-light-bootstrap-dashboard/img/faces/face-5.jpg"
+  },
+  {
+    "revision": "0e953b1ea6d6e3addd7210e9c7c420a7",
+    "url": "/pvue-light-bootstrap-dashboard/img/faces/face-2.jpg"
+  },
+  {
+    "revision": "43b98081492ac3bcb4a1fac6cf709403",
+    "url": "/pvue-light-bootstrap-dashboard/img/faces/tim_vector.jpe"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/pvue-light-bootstrap-dashboard/img/favicon.png"
+  },
+  {
+    "revision": "0b68eb8f1cde1fc9987a9196df05b96a",
+    "url": "/pvue-light-bootstrap-dashboard/img/faces/face-4.jpg"
+  },
+  {
+    "revision": "f575a04ebbb31b5798a4c54783e745a2",
+    "url": "/pvue-light-bootstrap-dashboard/img/new_logo.png"
+  },
+  {
+    "revision": "d27fbc90c2e644dfdc9765640dc713b9",
+    "url": "/pvue-light-bootstrap-dashboard/img/mask.png"
+  },
+  {
+    "revision": "0a337c37f03c0462996b7b03758fa72b",
+    "url": "/pvue-light-bootstrap-dashboard/img/loading-bubbles.svg"
+  },
+  {
+    "revision": "897b4cf909210560a84398d36da51983",
+    "url": "/pvue-light-bootstrap-dashboard/img/sidebar-4.jpg"
+  },
+  {
+    "revision": "9ebea76ee1225f00d882b21547a7b49f",
+    "url": "/pvue-light-bootstrap-dashboard/img/sidebar-5.jpg"
+  },
+  {
+    "revision": "44bf13a71a4db6e15913fe8af9296711",
+    "url": "/pvue-light-bootstrap-dashboard/img/tim_80x80.png"
+  },
+  {
+    "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
+    "url": "/pvue-light-bootstrap-dashboard/img/sidebar-2.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pvue-light-bootstrap-dashboard/robots.txt"
+  },
+  {
+    "revision": "ae31ea18d827b60c2e2139838d5dd387",
+    "url": "/pvue-light-bootstrap-dashboard/assets/fonts/maicons.svg"
+  },
+  {
+    "revision": "cd253e23ed052deeb80b42d2ed772183",
+    "url": "/pvue-light-bootstrap-dashboard/img/sidebar-3.jpg"
+  },
+  {
+    "revision": "c2a605fbc0e687b2e1b4b90a7c445cdd",
+    "url": "/pvue-light-bootstrap-dashboard/img/vue-logo.png"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/pvue-light-bootstrap-dashboard/img/sidebar-1.jpg"
+  }
+];
